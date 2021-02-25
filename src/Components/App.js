cb029259706eef7cbeb0f58eae8b672d0ce57fdc
rextejiri <rextejiri@gmail.com>
2021-02-25 11:11:36 -0600

    intro section added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './Header.js'
+import Intro from './Intro.js'
+
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Intro />
     </div>
   );
 }
--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
 
 
 
 export default function Header() {
+  const subtitle = "HI, I’M TEJIRI IBIHWIORI"
+  const introMsg = "A very hard working, meticulous developer with an art & design background. I create delightful and intuitive human-centered experiences at scale."
 
 
   return (
-    <div className="header">
-    <SendIcon className="sendIcon" fontSize="large"/>
+    <div className="Intro">
+      <Typography className="subtitle">{subtitle}</Typography>
+      <Typography className="introMsg">{introMsg}</Typography>
+      <div className="dropDown">
+        <ExpandMore />
+        <ExpandMore />
+        <ExpandMore />
+        <ExpandMore />
+      </div>
     </div>
   );
 
