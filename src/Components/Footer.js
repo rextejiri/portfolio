ddd5b5e7fca84f47c0756a585bce176a348f422c
rextejiri <rextejiri@gmail.com>
2021-03-01 14:53:04 -0600

    footer coming through nicely

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,39 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 
 
 export default function Footer() {
 
+const contacts = [
+  {name: "LinkedIn", Icon: faLinkedin},
+  {name: "GitHub", Icon: faGithub},
+  {name: "Discord", Icon: faDiscord},
+]
 
   return (
     <div className="footer">
      <hr />
-      <h4>tejirichuks@gmail.com</h4>
+    <div className="contact">
+      <h4>©2021</h4>
+      <div className="contactIcons">
+        {
+          contacts.map((contact, index)=> {
+            return(
+              <div className="contactIcon">
+                <FontAwesomeIcon
+                  icon={contact.Icon}
+                  title={contact.name}
+                  alt={contact.name}
+                />
+              </div>
+            );
+          })
+        }
+      </div>
+    </div>
     </div>
 
   );
