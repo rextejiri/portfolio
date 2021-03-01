07fc0a7f7b8e6758bbc1f1bdd071a0e38a1409a7
rextejiri <rextejiri@gmail.com>
2021-03-01 13:06:14 -0600

    project section looks good

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,35 +9,31 @@ import Dashboard from './assets/images/Dashboard.png';
 export default function Projects() {
 	return (
 		<React.Fragment>
+		<h1 className="projectFrame technology">Some projects I've Worked On:</h1>
 			<div className="projects">
-      <h2>Some projects I've Worked On:</h2>
 				<div className="card">
             <div className="img-card">
-              <h4>E-Commerce Website</h4>
-            	<img className="project-img" alt="Dashboard" />
+            	<img src={EthanCandleLogo} className="project-img" alt="Dashboard" />
             </div>
-            <p></p>
+						<h4>E-Commerce Website</h4>
         </div>
         <div className="card">
           <div className="img-card">
-            <h4>Bestie Social Media</h4>
-          	<img className="project-img" alt="Dashboard" />
+          	<img src={BestieLogo} className="project-img" alt="Dashboard" />
+						<h4>Social Media</h4>
           </div>
-            <p></p>
         </div>
+				<div className="card">
+					<div className="img-card">
+						<img src={Dashboard} className="project-img" alt="Dashboard" />
+					</div>
+					<h4>Dashboard</h4>
+				</div>
         <div className="card">
           <div className="img-card">
-            <h4>Weather App</h4>
-            <img className="project-img" alt="Dashboard" />
+            <img src={TwistLogo} className="project-img" alt="Dashboard" />
           </div>
-            <p></p>
-        </div>
-        <div className="card">
-          <div className="img-card">
-            <h4>Dashboard</h4>
-            <img className="project-img" alt="Dashboard" />
-          </div>
-            <p></p>
+					<h4>Weather App</h4>
         </div>
 			</div>
 		</React.Fragment>
