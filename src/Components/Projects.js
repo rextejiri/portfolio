c68772ac22369763f0964a43ec1efe36695835a8
rextejiri <rextejiri@gmail.com>
2021-03-09 10:28:41 -0600

    href links added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,30 +11,38 @@ export default function Projects() {
 		<React.Fragment>
 		<h1 className="projectFrame technology">Some projects I've Worked On:</h1>
 			<div className="projects">
+			<a href="https://github.com/rextejiri/testheroku">
 				<div className="card">
             <div className="img-card">
             	<img src={EthanCandleLogo} className="project-img" alt="Dashboard" />
             </div>
 						<h4>E-Commerce Website</h4>
         </div>
-        <div className="card">
-          <div className="img-card">
-          	<img src={BestieLogo} className="project-img" alt="Dashboard" />
+				</a>
+				<a href="https://social-media-app-1.herokuapp.com/">
+				<div className="card">
+					<div className="img-card">
+						<img src={BestieLogo} className="project-img" alt="Dashboard" />
 						<h4>Social Media</h4>
-          </div>
-        </div>
+					</div>
+				</div>
+				</a>
+				<a href="https://github.com/rextejiri/dashboard-app">
 				<div className="card">
 					<div className="img-card">
 						<img src={Dashboard} className="project-img" alt="Dashboard" />
 					</div>
 					<h4>Dashboard</h4>
 				</div>
+				</a>
+				<a href="https://rextejiri.github.io/The-Twister/">
         <div className="card">
           <div className="img-card">
             <img src={TwistLogo} className="project-img" alt="Dashboard" />
           </div>
 					<h4>Weather App</h4>
         </div>
+				</a>
 			</div>
 		</React.Fragment>
 	);
